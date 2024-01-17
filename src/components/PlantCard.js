5433ba134280f0e06bf49a8ba7c3496a9240d4dc
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-17 11:19:20 -0700

    Advanced Deliverable #1 attempted-not working, update price of plant

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function PlantCard({ plant, removePlant }) {
   const [stock, setStock] = useState(true)
+  const [price, setPrice] = useState(plant.price)
 
   function handleDelete() {
     fetch(`http://localhost:6001/plants/${plant.id}`, {
@@ -10,17 +11,36 @@ function PlantCard({ plant, removePlant }) {
     removePlant(plant.id)
   }
 
+  function handlePriceChange(e) {
+    setPrice(e.target.value)
+  }
+
+  function handlePriceSubmit(e) {
+    e.preventDefault()
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({price: price})
+    })
+    handlePriceChange()
+  }
+
   return (
     <li className="card">
       <img src={plant.image} alt={"plant name"} />
       <h4>{plant.name}</h4>
-      <p>Price: {plant.price}</p>
+      <p>
+        Price: {plant.price}
+        <button onClick={() => setPrice(handlePriceSubmit)} className="edit-plant-price">Edit price</button>
+      </p>
       {stock ? (
         <button onClick={() => setStock(false)} className="primary" >In Stock</button>
       ) : (
         <button onClick={() => setStock(true)} className="secondary" >Out of Stock</button>
       )}
-      <button onClick={handleDelete} className="emoji-button delete">🗑</button>
+      <button onClick={handleDelete} className="remove-plant">Delete Plant</button>
     </li>
   );
 }
