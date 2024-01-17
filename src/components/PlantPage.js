517b31375d684bc0f58986e63d369b593ede3ebc
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-17 10:46:54 -0700

    All core deliverables met

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  console.log(plants)
+  console.log(search)
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(response => response.json())
+      .then(setPlants)
+  }, [])
+
+  function handleNewPlantFormSubmit(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  const displayedPlants = plants.filter((plants) => plants.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onNewPlantFormSubmit={handleNewPlantFormSubmit}/>
+      <Search search={setSearch} setSearch={setSearch}/>
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function Search() {
+function Search({ search, setSearch }) {
+
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
@@ -8,7 +10,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        value={search} 
+        onChange={(e) => setSearch(e.target.value)}
       />
     </div>
   );
